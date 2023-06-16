cdf2c0b608c47a4a6e04022e12b4e4cd9fea9d9b
Zeekhona <1996zikhona@gmail.com>
2023-06-16 04:14:53 -0700

    lasd didgit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,15 +13,15 @@ n = rand() - RAND_MAX / 2;
 lastd = n % 10;
 if (lastd > 5)
 {
-printf("The last digit of %d is %d and is greater than 5\n", n, lastd);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 }
-else if(lastd == 0)
+else if (lastd == 0)
 {
-printf("The last digit of %d is %d and is zero\n", n, lastd);
+printf("Last digit of %d is %d and is zero\n", n, lastd);
 }
 else
 {
-printf("The last digit of %d is %d and is less than 6 not 0\n", n, lastd);
+printf("Last digit of %d is %d and is less than 6 not 0\n", n, lastd);
 }
 return (0);
 }
