c8f538c410316ac31e894738712826eb62af6658
Zeekhona <1996zikhona@gmail.com>
2023-06-16 05:04:35 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int n;
+for(n = 0; n <= 9; n++)
+{
+putchar(n + 48);
+}
+putchar('\n');
+return (0);
+}
