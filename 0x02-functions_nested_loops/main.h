8dcbc3ccfafbd4431b1764235f096973fffdc3f7
Zeekhona <1996zikhona@gmail.com>
2023-06-20 02:12:58 -0700

    1. I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabets_x10(void);
@@ -11,3 +13,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+#endif
