7ef935fbc74246ca3dcadca909486a7e0c8c0140
Zeekhona <1996zikhona@gmail.com>
2023-07-19 03:44:47 -0700

    Whats my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,26 +3,20 @@
 #include <stdio.h>
 
 /**
-
  * print_name - print name using pointer to function
-
  * @name: string to add
-
  * @f: pointer to function
-
  * Return: nothing
-
  **/
 
 void print_name(char *name, void (*f)(char *))
 
 {
 
-        if (name == NULL || f == NULL)
+	if (name == NULL || f == NULL)
 
-                return;
+		return;
 
 
-        f(name);
-
+	f(name);
 }
