fef2634d4b92046acc3ad3e5230f13dd09dbb6fe
Zeekhona <1996zikhona@gmail.com>
2023-07-19 03:51:36 -0700

    If you spend too much time thinking about a thing

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,37 +3,31 @@
 #include <stdio.h>
 
 /**
-
  * array_iterator - prints each array elem on a newl
-
  * @array: array
-
  * @size: how many elem to print
-
  * @action: pointer to print in regular or hex
-
  * Return: void
-
- */
+ **/
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 
 {
 
-        unsigned int i;
+	unsigned int i;
 
 
-        if (array == NULL || action == NULL)
+	if (array == NULL || action == NULL)
 
-                return;
+		return;
 
 
-        for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 
-        {
+	{
 
-                action(array[i]);
+		action(array[i]);
 
-        }
+	}
 
 }
